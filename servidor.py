40738205fb935cd87f33fa43ea4c2ab1309b3280
Erika <a01745158@itesm.mx>
2022-10-25 09:59:55 -0500

    Cambios servidor

--- servidor.py
+++ servidor.py
@@ -1,6 +1,14 @@
 from crypt import methods
-from distutils.log import debug
+import fileinput
+from fileinput import filename
+
 from flask import Flask, request, jsonify, render_template
+# Sirve para trabajar con los archivos que nos llegan de internet
+from werkzeug.utils import secure_filename
+# Sirve para la inteligencia artificial
+from joblib import load
+import numpy as np
+import os
 
 
 # Generar el servidor en Flask (Back-end)
@@ -13,6 +21,34 @@ servidorWeb = Flask(__name__)
 @servidorWeb.route("/test", methods=['GET'])
 def formulario():
     return render_template('pagina.html')
+
+# Agregar otro servicio. Procesar datos a través del Form
+
+
+@servidorWeb.route('/modeloIA', methods=["POST"])
+def modeloForm():
+    # Procesar datos de entrada
+    contenido = request.form
+
+    print(contenido)
+
+    return jsonify({"Resultado": "datos recibidos"})
+
+# Procesar datos de un archivo
+
+
+@servidorWeb.route('/modeloFile', methods=["POST"])
+def modeloFile():
+    f = request.files['file']
+    # PAra poder trabajr con el filename
+    filename = secure_filename(f.filename)
+    path = os.path.join(os.getcwd, 'files', filename)
+    f.save(path)
+    file = open(path, 'r')
+
+    for line in file:
+        print(line)
+    return jsonify({"Resultado": "datos recibidos archivo"})
 
 
 if __name__ == '__main__':
